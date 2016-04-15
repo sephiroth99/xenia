422fae5bdaa656c9bffa87a5ddf79dbc8885bdd3
sephiroth99 <sephiroth99@gmail.com>
2016-04-15 01:37:39 -0400

    initial linux debugging

---
+++ src/xenia/base/debugging_linux.cc
@@ -0,0 +1,31 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2016 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include "xenia/base/debugging.h"
+
+#include "xenia/base/string_buffer.h"
+
+namespace xe {
+namespace debugging {
+
+bool IsDebuggerAttached() {
+  // TODO(sephiroth99)
+  return false;
+}
+
+void Break() {
+  // TODO(sephiroth99)
+}
+
+void DebugPrint(const char* fmt, ...) {
+  // TODO(sephiroth99)
+}
+
+}  // namespace debugging
+}  // namespace xe
