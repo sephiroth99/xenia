b44fbf26d0dab26c599c33ead4583ee1a73efdf5
sephiroth99 <sephiroth99@gmail.com>
2016-04-15 01:32:26 -0400

    initial posix mapped memory

--- src/xenia/base/mapped_memory_posix.cc
+++ src/xenia/base/mapped_memory_posix.cc
@@ -11,6 +11,7 @@
 
 #include <sys/mman.h>
 #include <cstdio>
+#include <mutex>
 
 #include "xenia/base/string.h"
 
@@ -72,6 +73,99 @@ std::unique_ptr<MappedMemory> MappedMemory::Open(const std::wstring& path,
   }
 
   return std::move(mm);
+}
+
+class PosixChunkedMappedMemoryWriter : public ChunkedMappedMemoryWriter {
+ public:
+  PosixChunkedMappedMemoryWriter(const std::wstring& path, size_t chunk_size,
+                                 bool low_address_space)
+      : ChunkedMappedMemoryWriter(path, chunk_size, low_address_space) {}
+
+  ~PosixChunkedMappedMemoryWriter() override {
+    std::lock_guard<std::mutex> lock(mutex_);
+    chunks_.clear();
+  }
+
+  uint8_t* Allocate(size_t length) override {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (!chunks_.empty()) {
+      uint8_t* result = chunks_.back()->Allocate(length);
+      if (result != nullptr) {
+        return result;
+      }
+    }
+    auto chunk = std::make_unique<Chunk>(chunk_size_);
+    auto chunk_path = path_ + L"." + std::to_wstring(chunks_.size());
+    if (!chunk->Open(chunk_path, low_address_space_)) {
+      return nullptr;
+    }
+    uint8_t* result = chunk->Allocate(length);
+    chunks_.push_back(std::move(chunk));
+    return result;
+  }
+
+  void Flush() override {
+    std::lock_guard<std::mutex> lock(mutex_);
+    for (auto& chunk : chunks_) {
+      chunk->Flush();
+    }
+  }
+
+  void FlushNew() override {
+    std::lock_guard<std::mutex> lock(mutex_);
+    for (auto& chunk : chunks_) {
+      chunk->FlushNew();
+    }
+  }
+
+ private:
+  class Chunk {
+   public:
+    explicit Chunk(size_t capacity)
+        : data_(nullptr),
+          offset_(0),
+          capacity_(capacity),
+          last_flush_offset_(0) {}
+
+    ~Chunk() {
+      if (data_) {
+        // TODO(sephiroth99)
+      }
+    }
+
+    bool Open(const std::wstring& path, bool low_address_space) {
+      // TODO(sephiroth99)
+      return false;
+    }
+
+    uint8_t* Allocate(size_t length) {
+      // TODO(sephiroth99)
+      return nullptr;
+    }
+
+    void Flush() {
+      // TODO(sephiroth99)
+    }
+
+    void FlushNew() {
+      // TODO(sephiroth99)
+    }
+
+   private:
+    uint8_t* data_;
+    size_t offset_;
+    size_t capacity_;
+    size_t last_flush_offset_;
+  };
+
+  std::mutex mutex_;
+  std::vector<std::unique_ptr<Chunk>> chunks_;
+};
+
+std::unique_ptr<ChunkedMappedMemoryWriter> ChunkedMappedMemoryWriter::Open(
+    const std::wstring& path, size_t chunk_size, bool low_address_space) {
+  // TODO(sephiroth99)
+  return nullptr;
 }
 
 }  // namespace xe
