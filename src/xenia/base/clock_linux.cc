e4709616f91c3cdd86b066c68883546b4977ce69
sephiroth99 <sephiroth99@gmail.com>
2016-04-15 01:37:08 -0400

    initial linux clock

---
+++ src/xenia/base/clock_linux.cc
@@ -0,0 +1,34 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2015 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+#include "xenia/base/clock.h"
+
+namespace xe {
+
+uint64_t Clock::host_tick_frequency() {
+  // TODO(sephiroth99)
+  return 0;
+}
+
+uint64_t Clock::QueryHostTickCount() {
+  // TODO(sephiroth99)
+  return 0;
+}
+
+uint64_t Clock::QueryHostSystemTime() {
+  // TODO(sephiroth99)
+  return 0;
+}
+
+uint32_t Clock::QueryHostUptimeMillis() {
+  // TODO(sephiroth99)
+  return 0;
+}
+
+}  // namespace xe
