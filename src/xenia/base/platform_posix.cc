e75d96b8ef0e318861afe88a200be265be107dc3
sephiroth99 <sephiroth99@gmail.com>
2017-06-05 22:04:55 -0400

    platform

---
+++ src/xenia/base/platform_posix.cc
@@ -0,0 +1,16 @@
+/**
+ ******************************************************************************
+ * Xenia : Xbox 360 Emulator Research Project                                 *
+ ******************************************************************************
+ * Copyright 2017 Ben Vanik. All rights reserved.                             *
+ * Released under the BSD license - see LICENSE in the root for more details. *
+ ******************************************************************************
+ */
+
+namespace xe {
+
+void LaunchBrowser(const char* url) {
+  // TODO(sephiroth99)
+}
+
+}  // namespace xe
